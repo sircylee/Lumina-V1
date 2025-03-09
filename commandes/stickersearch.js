aee5100a6dff92d2a611dd1ea9d632c3ab1d75cd
sircylee <kylie.x.cylee.technology@gmail.com>
2025-03-09 08:21:00 +0200

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -5,7 +5,7 @@ const {zokou} = require("../framework/zokou");
 zokou({
   nomCom: "stickersearch",
   categorie: 'Search',
-  reaction: "🍁"
+  reaction: "👾"
 },
 async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg, nomAuteurMessage } = commandeOptions;
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'Zokou-MD',
+      author: 'Lumina 👾❤️',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
